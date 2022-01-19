91707dac8382b2107c92959511ef8aac2f3cad7a
Alex Zissis <alex@zico.dev>
2022-01-19 21:12:50 +1100

    develop (#2)
    * add pr check flow
    
    * add pr checks
    
    * add pr checks
    
    * add pr checks

---
+++ .github/actions/build-and-test-container/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd $APP_PATH
+
+if [[ "$RUN_TESTS" -eq "1" ]]; then
+    docker build -t $APPLICATION_NAME:latest . --target test
+fi
+
+HOST_PORT="10000"
+
+docker build -t $APPLICATION_NAME:latest .
+docker run -e DISABLE_LOGS=1 -d -p $HOST_PORT:$PORT $APPLICATION_NAME:latest
+
+sleep 5
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" localhost:$HOST_PORT/health)
+if [ $STATUS_CODE -ne "200" ]; then echo "Healthcheck failed with code: $STATUS_CODE"; exit 1; fi
