efd363abafd649f8650d4fe3467894b4ff2c456b
Alex Zissis <alex@zico.dev>
2022-04-25 11:19:09 +1000

    fix brackets

--- .github/actions/build-and-test-container/script.sh
+++ .github/actions/build-and-test-container/script.sh
@@ -2,19 +2,19 @@
 
 cd $APP_PATH
 
-if [[ "$RUN_TESTS" -eq "1" ]]; then
+if [ "$RUN_TESTS" -eq "1" ]; then
     docker build -t $APPLICATION_NAME:latest . --target test
 fi
 
 HOST_PORT="10000"
 
-if [[ ! -z "$BUILD_SCRIPT" ]]; then
+if [ ! -z "$BUILD_SCRIPT" ]; then
     $BUILD_SCRIPT
 else
     docker build -t $APPLICATION_NAME:latest .
 fi
 
-if [[ ! -z "$RUN_SCRIPT" ]]; then
+if [ ! -z "$RUN_SCRIPT" ]; then
     $RUN_SCRIPT $HOST_PORT
 else
     docker run -e DISABLE_LOGS=1 -d -p $HOST_PORT:$PORT $APPLICATION_NAME:latest
